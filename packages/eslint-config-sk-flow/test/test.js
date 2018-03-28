5e313e89d58880b08cfbab749d23f5c2927c03af
Sönke Kluth <mail@soenkekluth.com>
2018-03-28 04:28:12 +0200

    tests

--- packages/eslint-config-sk-flow/test/test.js
+++ packages/eslint-config-sk-flow/test/test.js
@@ -1,17 +1,16 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const eslint = require('eslint');
+const path = require('path');
+
+const srcPath = path.resolve(__dirname, '../');
 
 describe('test eslint config', () => {
-  beforeAll(async () => {
-    // console.log('jestPath', jestPath);
-  });
-
   it('eslint should pass without errors and no warnings', async () => {
-    const input = ['index.js', 'test/test.js'];
+    const input = [path.resolve(srcPath, 'index.js'), path.resolve(__dirname, 'test.js')];
 
     const output = new eslint.CLIEngine({
       envs: ['es6', 'node'],
-      configFile: 'index.js',
+      configFile: path.resolve(srcPath, 'index.js'),
       useEslintrc: false,
     }).executeOnFiles(input);
 

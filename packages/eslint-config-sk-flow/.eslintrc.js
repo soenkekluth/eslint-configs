bcafb3940c2762350c7aa358019e0cc53c30fb50
Sönke Kluth <mail@soenkekluth.com>
2018-03-30 19:45:02 +0200

    optimize flow

--- packages/eslint-config-sk-flow/.eslintrc.js
+++ packages/eslint-config-sk-flow/.eslintrc.js
@@ -3,16 +3,18 @@ module.exports = {
     'eslint-config-sk-common',
     'plugin:flowtype/recommended',
     'plugin:react/recommended',
-    // 'plugin:node/recommended',
     'prettier/flowtype',
     'prettier/react',
   ],
-  plugins: ['html', 'flowtype', 'react'],
+  plugins: ['babel', 'flowtype', 'flowtype-errors', 'private-props', 'html', 'react'],
   globals: {
     browser: true,
   },
   env: {
     browser: true,
+  },
+  rules: {
+    'flowtype-errors/show-errors': 2,
   },
   settings: {
     targets: ['last 2 versions'],
